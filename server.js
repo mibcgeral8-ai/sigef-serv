2820aca2142d59d467e0a96bb0249d59401a25c5
mibcgeral8-ai <mibcgeral8@gmail.com>
2026-02-11 12:40:13 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('SIGEF-SERV Online - Sistema Ativo');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
